ad146d49e06ec62b4b6c05bb4a84224699cdbc3b
sbelzile-r3d <sebastien.belzile@r3d.com>
2019-07-08 15:27:41 -0400

    Adds part of solution

--- 4.redux/src/board/BoardConnected.tsx
+++ 4.redux/src/board/BoardConnected.tsx
@@ -2,13 +2,18 @@ import { IApplicationState } from "../rootReducer";
 import { IBoardStateProps, IBoardDispatchProps } from "./Board";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import { clickSquare } from "./BoardActions";
 
 const mapStateToProps = (state: IApplicationState): IBoardStateProps => {
-
+  return {
+    squares: state.board.squares
+  }
 }
 
 const mapDispatchToProps = (dispatch: Dispatch): IBoardDispatchProps => {
-
+  return {
+    onClick: (id: number) => dispatch(clickSquare(id))
+  };
 }
 
 export const BoardConnected = connect(mapStateToProps, mapDispatchToProps)
--- 4.redux/src/board/BoardReducers.ts
+++ 4.redux/src/board/BoardReducers.ts
@@ -1,12 +1,33 @@
 import { IReduxAction } from "../utils/ReduxUtils";
-import { IBoardActionPayload } from "./BoardActions";
+import { IBoardActionPayload, BoardActions } from "./BoardActions";
 
 export interface IBoardState {
   squares: string[],
   xIsNext: boolean
 }
 
-export const boardReducer = (state: IBoardState, action: IReduxAction<IBoardActionPayload>): IBoardState => ({
+const INITIAL_STATE: IBoardState = {
   squares: Array(9).fill(''),
   xIsNext: true
-});
+}
+
+export const boardReducer = (state: IBoardState = INITIAL_STATE, action: IReduxAction<IBoardActionPayload>): IBoardState => {
+  switch (action.type) {
+    case BoardActions.reset:
+      return {
+        squares: Array(9).fill(''),
+        xIsNext: true
+      }
+    case BoardActions.clickSquare:
+      const squares = state.squares.slice();
+      if (action.payload && action.payload.id) {
+        squares[action.payload.id] = state.xIsNext ? 'X' : 'O';
+      }
+      return {
+        squares,
+        xIsNext: !state.xIsNext
+      }
+    default:
+      return state;
+  }
+};
